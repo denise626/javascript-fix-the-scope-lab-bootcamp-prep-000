71c029f957b36f5f9785c07a58b3007c93d989f1
April Kiser <denise626@hotmail.com>
2018-04-28 17:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ function yourAnimal() {
 
 
 function add2(n) {
-  return n + two
 
-  // Feel free to move things around!
   const two = 2
+  // Feel free to move things around!
+  
+  return n + two
 }
 
 var funkyFunction = function() {
